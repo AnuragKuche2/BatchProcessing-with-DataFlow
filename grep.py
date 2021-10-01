6f4865ead0bee218ee62a10f463770f8f2aa36ed
Anurag kuche <33677947+AnuragKuche2@users.noreply.github.com>
2021-10-01 16:25:20 -0400

    Add files via upload

--- grep.py
+++ grep.py
@@ -3,35 +3,51 @@
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.io.gcp.internal.clients import bigquery
+from apache_beam.transforms.combiners import ToList
+
 
 class Split(beam.DoFn):
    def process(self, element):
-
       line_ = element.split(',')
       if len(line_) == 16:
          return [(line_[5], 1 )]
 
 table_schema = {'fields': [
-    {'name': 'Neighbourhood', 'type': 'STRING', 'mode': 'REQUIRED'},
-   {'name': 'Count', 'type': 'INTEGER', 'mode': 'REQUIRED'}
+    {'name': 'Neighbourhood', 'type': 'STRING'},
+   {'name': 'Count', 'type': 'INTEGER'}
 ]}
 
+def addKey(row):
+    return (1, row)
+
+def sortGroupedData(row):
+    (keyNumber, sortData) = row
+    sortData.sort(key=lambda x: x[1], reverse=True)
+    return sortData
+
+
 if __name__ == '__main__':
-   options = PipelineOptions()
-   p = beam.Pipeline(options=options)
-   input = 'AB_NYC_2019.csv'
-   output_prefix = 'test_op123'
+    options = PipelineOptions()
+    p = beam.Pipeline(options=options)
+    inputfile = 'gs://qwiklabs-gcp-02-0b434aff0c49/AB_NYC_2019.csv'
+    output_prefix = 'gs://qwiklabs-gcp-02-0b434aff0c49/test_op11'
 
-   table_spec = bigquery.TableReference(
-      projectId='yourprojectname',
-      datasetId='yourdatasetid',
-      tableId='yourtable')
 
-   rows = (p |
-           beam.io.ReadFromText(input, skip_header_lines=True) |
-           beam.ParDo(Split()) | beam.combiners.Count.PerKey() | beam.io.WriteToText(output_prefix))
+    table_spec = bigquery.TableReference(
+      projectId='qwiklabs-gcp-02-0b434aff0c49',
+      datasetId='qwiklabs-gcp-02-0b434aff0c49:datasetid',
+      tableId= "qwiklabs-gcp-02-0b434aff0c49:datasetid.tablename")
 
-   bq = (
+
+    rows = (p |
+            beam.io.ReadFromText(inputfile, skip_header_lines=True) |
+            beam.ParDo(Split()) | beam.combiners.Count.PerKey()
+            | 'AddKey' >> beam.Map(addKey)
+            | 'GroupByKey' >> beam.GroupByKey()
+            | 'SortGroupedData' >> beam.Map(sortGroupedData)
+            | beam.io.WriteToText(output_prefix))
+
+    bq = (
            rows
            | 'Writes To BigQuery' >> beam.io.WriteToBigQuery(
       table_spec,
@@ -40,4 +56,4 @@ if __name__ == '__main__':
       create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED
    ))
 
-   p.run().wait_until_finish()
+    p.run().wait_until_finish()
